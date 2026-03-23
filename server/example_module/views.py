108780855b2502a2fa212f879895c28bd2dca217
Jzackiewicz <jj.zackiewicz@gmail.com>
2026-03-23 01:36:02 +0100

    folder rename

--- server/example_module/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-def dbozic(request):
-    return (HttpResponse("I love Damian <3"))
-
-def dbozic_charm(request):
-    return (HttpResponse("He has a pretty smile <3"))
--- server/game/apps.py
+++ server/game/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class ExampleModuleConfig(AppConfig):
-    name = 'example_module'
+    name = 'game'
