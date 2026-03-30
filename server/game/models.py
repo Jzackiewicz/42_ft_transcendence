20818629bcb813f43f98ffadd27f5516b9170285
Maria Budkevich <iammariatimoshenko@gmail.com>
2026-03-30 14:59:24 +0200

    replace previous db with Postgres. add Question model. setup .env.example

--- server/game/models.py
+++ server/game/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    text = models.CharField(max_length=255)
+    answer_a = models.CharField(max_length=255)
+    answer_b = models.CharField(max_length=255)
+    answer_c = models.CharField(max_length=255)
+    answer_d = models.CharField(max_length=255)
+    correct_answer = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.text
