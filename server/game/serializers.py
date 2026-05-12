780e2128d845169b76378b3517933b402b2c2222
Jzackiewicz <jj.zackiewicz@gmail.com>
2026-05-12 16:11:21 +0200

    Added more strict answer payload validation

--- server/game/serializers.py
+++ server/game/serializers.py
@@ -4,45 +4,56 @@ from .models import Question
 from .models import GameSession, SessionPlayer, SessionQuestion
 
 
+class StrictSerializer(serializers.Serializer):
+	def validate(self, attrs):
+		unknown_fields = set(self.initial_data) - set(self.fields)
+		if unknown_fields:
+			raise serializers.ValidationError({
+				field: ["Unknown field."]
+				for field in sorted(unknown_fields)
+			})
+		return attrs
+
+
 class GameSessionOutputSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = GameSession
-        fields = ['session_uuid', 'current_status', 'max_players', 'created_at']
+	class Meta:
+		model = GameSession
+		fields = ['session_uuid', 'current_status', 'max_players', 'created_at']
 
 class SessionPlayerOutputSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SessionPlayer
-        fields = ['id', 'display_name', 'seat_number', 'lives', 'points', 'player_type']
+	class Meta:
+		model = SessionPlayer
+		fields = ['id', 'display_name', 'seat_number', 'lives', 'points', 'player_type']
 
 
-class SubmitAnswerPayloadSerializer(serializers.Serializer):
-    answer = serializers.CharField(
-        allow_null=True, allow_blank=True, required=False, default=None
-    )
+class SubmitAnswerPayloadSerializer(StrictSerializer):
+	answer = serializers.CharField(
+		allow_null=True, allow_blank=True, required=False, default=None
+	)
 
-class NominatePlayerPayloadSerializer(serializers.Serializer):
-    target_player_id = serializers.IntegerField(required=True)
+class NominatePlayerPayloadSerializer(StrictSerializer):
+	target_player_id = serializers.IntegerField(required=True)
 
 
 class PlayerSnapshotSerializer(serializers.ModelSerializer):
-    is_alive = serializers.BooleanField(read_only=True)
-    
-    class Meta:
-        model = SessionPlayer
-        fields = ['id', 'display_name', 'seat_number', 'lives', 'points', 'answered_count', 'is_alive']
+	is_alive = serializers.BooleanField(read_only=True)
+	
+	class Meta:
+		model = SessionPlayer
+		fields = ['id', 'display_name', 'seat_number', 'lives', 'points', 'answered_count', 'is_alive']
 
 class QuestionSnapshotSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Question
-        # UWAGA: Celowo omijamy 'correct_answer' by zapobiec oszustwom na frontendzie!
-        fields = ['question_text', 'category']
+	class Meta:
+		model = Question
+		# UWAGA: Celowo omijamy 'correct_answer' by zapobiec oszustwom na frontendzie!
+		fields = ['question_text', 'category']
 
 class SessionQuestionSnapshotSerializer(serializers.ModelSerializer):
-    question = QuestionSnapshotSerializer()
-    
-    class Meta:
-        model = SessionQuestion
-        fields = ['id', 'question', 'order_index']
+	question = QuestionSnapshotSerializer()
+	
+	class Meta:
+		model = SessionQuestion
+		fields = ['id', 'question', 'order_index']
 
 class GameStateSnapshotSerializer(serializers.ModelSerializer):
     players = PlayerSnapshotSerializer(source='session_players', many=True)
