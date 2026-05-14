cae9a4c00f918ca78139eea9f74e677c03aca9ff
Kvadrot <send.me.your.maid@gmail.com>
2026-05-14 18:37:12 +0200

    simple logout is done

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,12 @@
+import { useHomePage } from './useHomePage'
+
 function HomePage() {
+    const { user, handleLogout } = useHomePage()
+
     return (
-        <div class-name="home-page-container">
+        <div className="home-page-container">
             <h1>Home page</h1>
+            <button onClick={handleLogout}>Logout: {user?.username}</button>
         </div>
     )
 }
