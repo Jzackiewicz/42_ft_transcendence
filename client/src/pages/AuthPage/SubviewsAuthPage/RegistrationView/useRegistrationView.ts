8acba7876d78817ba0b71411234a18fd3647b719
Kvadrot <send.me.your.maid@gmail.com>
2026-05-19 22:22:42 +0200

    The font is inconsistent is fixed

--- client/src/pages/AuthPage/SubviewsAuthPage/RegistrationView/useRegistrationView.ts
+++ client/src/pages/AuthPage/SubviewsAuthPage/RegistrationView/useRegistrationView.ts
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {useUser} from '../../../../context/UserContext'
+import { useUser } from '../../../../context/UserContext'
 import { register, login } from '../../../../api/apiWrapper'
 
 
