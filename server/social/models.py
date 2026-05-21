4fa889ddaa98838a6116392f2ad305c4923e1859
Maria Budkevich <iammariatimoshenko@gmail.com>
2026-05-21 14:39:27 +0200

    added Friendship, FriendRequest models and ran migrations

--- server/social/models.py
+++ server/social/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class ChatMessage(models.Model):
@@ -6,3 +7,41 @@ class ChatMessage(models.Model):
     sender_username = models.CharField(max_length=255)
     message = models.TextField(max_length=500)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Friendship(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, 
+                             on_delete=models.CASCADE, 
+                             related_name='friendships')
+    friend = models.ForeignKey(settings.AUTH_USER_MODEL, 
+                               on_delete=models.CASCADE, 
+                               related_name='friend_of')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'friend')
+
+
+class FriendRequest(models.Model):
+    class Status(models.TextChoices):
+        PENDING = 'pending', 'Pending'
+        ACCEPTED = 'accepted', 'Accepted'
+        DECLINED = 'declined', 'Declined'
+
+    from_user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                  on_delete=models.CASCADE,
+                                  related_name='sent_friend_requests')
+
+    to_user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE,
+                                related_name='received_friend_requests')
+        
+    status = models.CharField(max_length=10,
+                              choices=Status.choices,
+                              default=Status.PENDING)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('from_user', 'to_user')
