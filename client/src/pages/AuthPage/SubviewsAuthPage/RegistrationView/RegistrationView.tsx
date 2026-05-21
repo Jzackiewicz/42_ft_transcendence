f10304e52f91636274fa6202258b4cd462fbc783
Kvadrot <send.me.your.maid@gmail.com>
2026-05-21 14:00:44 +0200

    registration prevalidation is done

--- client/src/pages/AuthPage/SubviewsAuthPage/RegistrationView/RegistrationView.tsx
+++ client/src/pages/AuthPage/SubviewsAuthPage/RegistrationView/RegistrationView.tsx
@@ -7,16 +7,15 @@ interface RegistrationProps {
 }
 
 function RegistrationView({onSuccess}: RegistrationProps) {
-    const { username, setUsername, email, setEmail, password, setPassword, handleRegister } = useRegistrationView(onSuccess)
-    const [confirmPassword, setConfirmPassword] = useState("")
+    const { username, setUsername, email, setEmail, password, setPassword, confPassword, setConfPassword, handleRegister, errors } = useRegistrationView(onSuccess)
 
     return (
         <div className="registration-view">
             <form onSubmit={(e) => {e.preventDefault(); handleRegister()}}> 
-                <InputField title= "Display Name" type="text" placeholder="Enter your nickname" value={username} onChange={(value) => setUsername(value)} />
-                <InputField title="Email" type="email" placeholder="your_email@gmail.com" value={email} onChange={(value) => setEmail(value)} />
-                <InputField title="Password" type="password" placeholder="Create a password" value={password} onChange={(value) => setPassword(value)} />
-                <InputField title="Confirm Password" type="password" placeholder="Confirm your password" value={confirmPassword} onChange={(value) => setConfirmPassword(value)} />
+                <InputField title= "Display Name" type="text" placeholder="Enter your nickname" value={username} onChange={(value) => setUsername(value)} error={errors.usernameIsEmptyErr} />
+                <InputField title="Email" type="email" placeholder="your_email@gmail.com" value={email} onChange={(value) => setEmail(value)} error={errors.mailIsEmptyErr} />
+                <InputField title="Password" type="password" placeholder="Create a password" value={password} onChange={(value) => setPassword(value)} error={errors.passIsEmptyErr || errors.passWeakErr} />
+                <InputField title="Confirm Password" type="password" placeholder="Confirm your password" value={confPassword} onChange={(value) => setConfPassword(value)} error={errors.confirmPassIsEmptyErr || errors.passDoesntMatchErr} />
                 <button type="submit" className="auth-submit"> Register </button>
             </form>
         </div>
