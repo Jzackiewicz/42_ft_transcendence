fcfccc7d3ccb572564ff1991589d8460711f088f
Kvadrot <send.me.your.maid@gmail.com>
2026-06-21 16:07:30 +0200

    since text is added to the badge

--- client/src/pages/HomePage/SubviewsHomePage/AccountHeader/AccountHeader.tsx
+++ client/src/pages/HomePage/SubviewsHomePage/AccountHeader/AccountHeader.tsx
@@ -113,7 +113,7 @@ function AccountHeader({ user, setUser, setShowJoinModal, setShowRulesModal, han
                 <div className={styles['account-field-row']}>{emailField}</div>
                 {errorMessage}
                 <div className={styles.accountBadges}>
-                    <Badge variant="human">{user?.date_joined?.slice(0, 10)}</Badge>
+                    <Badge variant="human">since: {user?.date_joined?.slice(0, 10)}</Badge>
                 </div>
             </div>
 
