c15647b8c85d6618951bfc00521349cf7715de43
Jzackiewicz <jj.zackiewicz@gmail.com>
2026-06-23 14:32:10 +0200

    Moved LobbyChat into GamePage sidebar

--- client/src/pages/GamePage/SubviewsGamePage/LobbyChat/LobbyChat.tsx
+++ client/src/pages/GamePage/SubviewsGamePage/LobbyChat/LobbyChat.tsx
@@ -1,9 +1,11 @@
 import { ChatInner } from '../../../../components/chat/ChatInner'
+import { cx } from '../../../../utils/cx'
+import shared from '../../../../components/chat/chat.module.css'
 import styles from './LobbyChat.module.css'
 
 export function LobbyChat() {
     return (
-        <div className={styles.lobbyChat}>
+        <div className={cx(shared.chatContainer, styles.lobbyChat)}>
             <ChatInner />
         </div>
     )
